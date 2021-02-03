f1090bd99f856467b729c50d178ba7950a2b5c59
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-03 21:50:38 +0100

    Integratidati mancanti del PM10, caricati dati del benzene

--- R/pm10.R
+++ R/pm10.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di pm10 per 6 stazioni della regione FRIULIVENEZIAGIULIA
+#' Tibble con i dati di pm10 per 16 stazioni della regione FRIULIVENEZIAGIULIA
 #'
-#' @format Un tibble con 8 colonne e 14795 osservazioni
+#' @format Un tibble con 8 colonne e 40036 osservazioni
 #'
 #' @usage
 #' pm10
