25d85449d28bb8203af1236c9518b490313ceb94
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-18 01:55:15 +0100

    Eliminati duplicati PM10 e aggiunti inquinanti co, o3

---
+++ R/co.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di co per 2 stazioni della regione FRIULIVENEZIAGIULIA
+#'
+#' @format Un tibble con 8 colonne e 5471 osservazioni
+#'
+#' @usage
+#' co
+"co"
--- R/pm10.R
+++ R/pm10.R
@@ -1,6 +1,6 @@
 #' Tibble con i dati di pm10 per 16 stazioni della regione FRIULIVENEZIAGIULIA
 #'
-#' @format Un tibble con 8 colonne e 40036 osservazioni
+#' @format Un tibble con 8 colonne e 38956 osservazioni
 #'
 #' @usage
 #' pm10
